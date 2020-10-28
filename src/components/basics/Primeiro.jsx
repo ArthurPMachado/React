0ae58bcaec6943e8cb80fc9e6acec1e7ee33a8c9
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-28 11:26:43 -0300

    Feat: add jsx to component

--- src/components/basics/Primeiro.jsx
+++ src/components/basics/Primeiro.jsx
@@ -1,5 +1,14 @@
+import React from 'react';
+
 function Primeiro() {
-  return 'Primeiro Componente';
+  const message = 'Seja bem vindo(a)';
+
+  return (
+    <div>
+      <h2>Primeiro Componente</h2>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default Primeiro;
