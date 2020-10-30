d0710ae97105c657a9a196e9614b56da823273f7
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-30 11:53:29 -0300

    Feat: add flexbox to cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,37 @@ import Fragmento from './components/basics/Fragmento';
 import NumeroAleatorio from './components/basics/NumeroAleatorio';
 import Card from './components/layout/Card';
 
+import './app.css';
+
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Fundamentos React</h1>
 
-      <Card titulo="#04 - Desafio Numero Aleatorio">
-        <NumeroAleatorio
-          min={10}
-          max={100}
-        />
-      </Card>
+      <div className="cards">
+        <Card titulo="#04 - Desafio Numero Aleatorio">
+          <NumeroAleatorio
+            min={10}
+            max={100}
+          />
+        </Card>
 
-      <Card titulo="#03 - Fragmento">
-        <Fragmento />
-      </Card>
+        <Card titulo="#03 - Fragmento">
+          <Fragmento />
+        </Card>
 
-      <Card titulo="#02 - Com Propriedades">
-        <Propriedades
-          titulo="Situação do Aluno"
-          aluno="Arthur Machado"
-          nota={9.3}
-        />
-      </Card>
+        <Card titulo="#02 - Com Propriedades">
+          <Propriedades
+            titulo="Situação do Aluno"
+            aluno="Arthur Machado"
+            nota={9.3}
+          />
+        </Card>
 
-      <Card titulo="#01 - Primeiro Componente">
-        <Primeiro />
-      </Card>
+        <Card titulo="#01 - Primeiro Componente">
+          <Primeiro />
+        </Card>
+      </div>
 
     </div>
   );
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import './card.css';
 
 function Card(props) {
-  const { titulo } = props;
+  const { titulo, children } = props;
 
   return (
-    <div className="container">
+    <div className="card-container">
       <div className="title">{titulo}</div>
       <div className="content">
-        {props.children}
+        {children}
       </div>
     </div>
   );
